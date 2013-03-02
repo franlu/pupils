cdafb3bab7bd766bd4c678ccb1deebbf91fdddc3
Your Name <you@example.com>
2013-03-02 12:53:35 +0100

    [FIX] Deprecated logging handler

--- pupils/settings.py
+++ pupils/settings.py
@@ -62,7 +62,7 @@ LOGIN_URL = "/usuario/login/"
 USE_I18N = True
 USE_L10N = True
 MEDIA_ROOT = os.path.join(PROJECT_ROOT,'media')
-MEDIA_URL = '/media'
+MEDIA_URL = '/media/'
 STATIC_ROOT = '' #os.path.join(PROJECT_ROOT,'static')
 STATIC_URL = '/static/'
 ADMIN_MEDIA_PREFIX = '/static/admin/'
@@ -116,15 +116,21 @@ INSTALLED_APPS = (
 
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
-# the site admins on every HTTP 500 error.
+# the site admins on every HTTP 500 error when DEBUG=False.
 # See http://docs.djangoproject.com/en/dev/topics/logging for
 # more details on how to customize your logging configuration.
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse'
+        }
+    },
     'handlers': {
         'mail_admins': {
             'level': 'ERROR',
+            'filters': ['require_debug_false'],
             'class': 'django.utils.log.AdminEmailHandler'
         }
     },
